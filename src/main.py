b2373905abe086991c872ea738aad7781a519deb
Jiahao Yuan <kaho0769@qq.com>
2024-10-05 02:05:30 +0800

    try find process

--- src/main.py
+++ src/main.py
@@ -8,6 +8,14 @@ os.mkdir("testdir")
 subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
 subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
 time.sleep(2)
+res = subprocess.run(
+    [
+        "cmd",
+        "/C",
+        'openfiles /query /fo table | find /I "testdir\\.venv\\Lib\\site-packages\\markupsafe\\_speedups.cp312-win_amd64.pyd"',
+    ],
+    capture_output=True,
+)
 shutil.rmtree("testdir")
 
 print(markupsafe.escape("Hello, <world>!"))
