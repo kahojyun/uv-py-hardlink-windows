037ea8f5c77cba836419df75d985c3936228bb0e
Jiahao Yuan <kaho0769@qq.com>
2024-10-05 01:55:23 +0800

    try sleep

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import markupsafe
 import subprocess
 import shutil
@@ -6,6 +7,7 @@ import os
 os.mkdir("testdir")
 subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
 subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
+time.sleep(2)
 shutil.rmtree("testdir")
 
 print(markupsafe.escape("Hello, <world>!"))
