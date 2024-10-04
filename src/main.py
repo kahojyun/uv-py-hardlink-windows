4beda5db76f2709bdfdc9eed720637a6e49e0737
Jiahao Yuan <kaho0769@qq.com>
2024-10-05 02:34:04 +0800

    uv verbose

--- src/main.py
+++ src/main.py
@@ -1,21 +1,11 @@
-import time
 import markupsafe
 import subprocess
 import shutil
 import os
 
 os.mkdir("testdir")
-subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
-subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
-time.sleep(2)
-res = subprocess.run(
-    [
-        "cmd",
-        "/C",
-        'openfiles /query /fo table | find /I "testdir\\.venv\\Lib\\site-packages\\markupsafe\\_speedups.cp312-win_amd64.pyd"',
-    ],
-    capture_output=True,
-)
+subprocess.run(["uv", "venv", "-v", ".venv"], cwd="./testdir")
+subprocess.run(["uv", "pip", "install", "-v", "markupsafe"], cwd="./testdir")
 shutil.rmtree("testdir")
 
 print(markupsafe.escape("Hello, <world>!"))
