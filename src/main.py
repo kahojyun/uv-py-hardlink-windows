0e2d6161e339111496ec03dfeb9c3481ceb4f0de
Jiahao Yuan <kaho0769@qq.com>
2024-10-05 02:45:02 +0800

    extra verbose

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import shutil
 import os
 
 os.mkdir("testdir")
-subprocess.run(["uv", "venv", "-v", ".venv"], cwd="./testdir")
-subprocess.run(["uv", "pip", "install", "-v", "markupsafe"], cwd="./testdir")
+subprocess.run(["uv", "venv", "-vvv", ".venv"], cwd="./testdir")
+subprocess.run(["uv", "pip", "install", "-vvv", "markupsafe"], cwd="./testdir")
 shutil.rmtree("testdir")
 
 print(markupsafe.escape("Hello, <world>!"))
