6a3366bc79aacfa8845906a4bb22cd6bae23775d
Jiahao Yuan <kaho0769@qq.com>
2024-10-06 01:33:21 +0800

    retry rmtree

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import markupsafe
 import subprocess
+import shutil
 import os
 
 os.mkdir("testdir")
 subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
 subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
-subprocess.run(["pwsh", "-Command", "{Remove-Item -Recurse -Force testdir}"])
+n = 5
+while n > 0:
+    try:
+        shutil.rmtree("testdir")
+    except PermissionError:
+        n -= 1
+        if n == 0:
+            raise
+    else:
+        break
 
 print(markupsafe.escape("Hello, <world>!"))
