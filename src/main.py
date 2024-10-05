894245e03807f45c9cf81330666d497d5fddc626
Jiahao Yuan <kaho0769@qq.com>
2024-10-06 01:40:48 +0800

    try remove dir with cmd

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
 import markupsafe
 import subprocess
-import shutil
 import os
 
 os.mkdir("testdir")
 subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
 subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
-n = 5
-while n > 0:
-    try:
-        shutil.rmtree("testdir")
-    except PermissionError:
-        n -= 1
-        if n == 0:
-            raise
-    else:
-        break
+subprocess.run(["cmd", "/C", "rmdir /S /Q testdir"])
 
 print(markupsafe.escape("Hello, <world>!"))
