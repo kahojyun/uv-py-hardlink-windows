d1031b8cd1994e4967dd7b229a757b9d0e5a2140
Jiahao Yuan <kaho0769@qq.com>
2024-10-06 01:51:39 +0800

    try os.system

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ import os
 os.mkdir("testdir")
 subprocess.run(["uv", "venv", ".venv"], cwd="./testdir")
 subprocess.run(["uv", "pip", "install", "markupsafe"], cwd="./testdir")
-subprocess.run(["cmd", "/C", "rmdir /S /Q testdir"])
+os.system("rmdir /S /Q testdir")
 
 print(markupsafe.escape("Hello, <world>!"))
